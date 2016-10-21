19c25aa2d288bba2904639898dab6db16ec9c642
SinisterRectus <sinister.rectus@gmail.com>
2016-10-20 22:04:56 -0400

    Optimizations and code cleanup

--- client/cSettlements.lua
+++ client/cSettlements.lua
@@ -1,3 +1,5 @@
+local unpack, insert = table.unpack, table.insert
+
 class 'Settlements'
 
 function Settlements:__init()
@@ -17,82 +19,66 @@ end
 
 function Settlements:Enter(args)
 
-	local trigger = self.triggers[args.trigger:GetId()]
-	if not trigger then return end
-	local settlement = trigger.settlement
+	local settlement = self.triggers[args.trigger:GetId()]
+	if not settlement then return end
 	local id = settlement.id
-	
+
 	self.counts[id] = self.counts[id] + 1
-	
+
 	if self.counts[id] == 1 then
-	
 		if args.entity.__type == "LocalPlayer" then
-		
 			if self.fire_local then
 				Events:Fire("LocalPlayerEnterSettlement", {
 					settlement = settlement
-				})			
+				})
 			end
-			
 			if self.fire_network then
 				Network:Send("Enter", {
 					id = id,
 					player = args.entity
 				})
 			end
-
 		elseif args.entity.__type == "Player" then
-		
 			if self.fire_local then
 				Events:Fire("PlayerEnterSettlement", {
 					settlement = settlement,
 					player = args.entity
 				})
 			end
-		
 		end
-		
 	end
 
 end
 
 function Settlements:Exit(args)
 
-	local trigger = self.triggers[args.trigger:GetId()]
-	if not trigger then return end
-	local settlement = trigger.settlement
+	local settlement = self.triggers[args.trigger:GetId()]
+	if not settlement then return end
 	local id = settlement.id
-	
+
 	self.counts[id] = self.counts[id] - 1
-	
+
 	if self.counts[id] == 0 then
-	
 		if args.entity.__type == "LocalPlayer" then
-		
 			if self.fire_local then
 				Events:Fire("LocalPlayerExitSettlement", {
 					settlement = settlement
-				})			
+				})
 			end
-			
 			if self.fire_network then
 				Network:Send("Exit", {
 					id = id,
 					player = args.entity
 				})
 			end
-
 		elseif args.entity.__type == "Player" then
-		
 			if self.fire_local then
 				Events:Fire("PlayerExitSettlement", {
 					settlement = settlement,
 					player = args.entity
 				})
 			end
-		
 		end
-		
 	end
 
 end
@@ -100,78 +86,75 @@ end
 function Settlements:ModuleLoad()
 
 	for id, data in ipairs(settlements) do
-	
+
 		if #data[4] == 3 then
-	
+
 			local settlement = {
 				id = id,
 				name = data[1],
-				angle = Angle(table.unpack(data[2])),
-				position = Vector3(table.unpack(data[3])),
+				angle = Angle(unpack(data[2])),
+				position = Vector3(unpack(data[3])),
 				triggers = {}
 			}
-			
+
 			local trigger = ShapeTrigger.Create({
 				angle = settlement.angle,
 				position = settlement.position,
-				components = {{size = Vector3(table.unpack(data[4])), type = data[5]}},
+				components = {{size = Vector3(unpack(data[4])), type = data[5]}},
 				trigger_player = true,
 				trigger_player_in_vehicle = true
 			})
-			
+
 			self.settlements[id] = settlement
-			self.triggers[trigger:GetId()] = {settlement = settlement}
-			table.insert(settlement.triggers, trigger)
-			
+			self.triggers[trigger:GetId()] = settlement
+			insert(settlement.triggers, trigger)
+
 		else
-		
+
 			local settlement = {
 				id = id,
 				name = data[1],
-				angle = Angle(table.unpack(data[2])),
-				position = Vector3(table.unpack(data[3])),
+				angle = Angle(unpack(data[2])),
+				position = Vector3(unpack(data[3])),
 				triggers = {}
 			}
-		
+
 			for i = 4, #data do
-			
+
 				local component = data[i]
-			
+
 				local trigger = ShapeTrigger.Create({
-					angle = Angle(table.unpack(component[1])),
-					position = settlement.position + settlement.angle * Vector3(table.unpack(component[2])),
-					components = {{size = Vector3(table.unpack(component[3])), type = component[4]}},
+					angle = Angle(unpack(component[1])),
+					position = settlement.position + settlement.angle * Vector3(unpack(component[2])),
+					components = {{size = Vector3(unpack(component[3])), type = component[4]}},
 					trigger_player = true,
 					trigger_player_in_vehicle = true
 				})
-				
-				self.triggers[trigger:GetId()] = {settlement = settlement}
-				table.insert(settlement.triggers, trigger)
-			
+
+				self.triggers[trigger:GetId()] = settlement
+				insert(settlement.triggers, trigger)
+
 			end
-			
+
 			self.settlements[id] = settlement
-		
+
 		end
-	
+
 		self.counts[id] = 0
 
 	end
-	
-	
+
 	settlements = nil
 	collectgarbage()
-	
+
 end
 
 function Settlements:ModuleUnload()
-
 	for _, settlement in pairs(self.settlements) do
 		for _, trigger in ipairs(settlement.triggers) do
 			trigger:Remove()
 		end
 	end
-
 end
 
 Settlements = Settlements()
--- server/sSettlements.lua
+++ server/sSettlements.lua
@@ -1,3 +1,5 @@
+local unpack = table.unpack
+
 class 'Settlements'
 
 function Settlements:__init()
@@ -13,54 +15,44 @@ end
 function Settlements:ModuleLoad()
 
 	for id, data in ipairs(settlements) do
-	
 		if #data[4] == 3 then
-	
-			local settlement = {
+			self.settlements[id] = {
 				id = id,
 				name = data[1],
-				angle = Angle(table.unpack(data[2])),
-				position = Vector3(table.unpack(data[3])),
+				angle = Angle(unpack(data[2])),
+				position = Vector3(unpack(data[3])),
 			}
-			
-			self.settlements[id] = settlement
-			
 		else
-		
-			local settlement = {
+			self.settlements[id] = {
 				id = id,
 				name = data[1],
-				angle = Angle(table.unpack(data[2])),
-				position = Vector3(table.unpack(data[3])),
+				angle = Angle(unpack(data[2])),
+				position = Vector3(unpack(data[3])),
 			}
-			
-			self.settlements[id] = settlement
-		
 		end
-	
 	end
-	
+
 	settlements = nil
 	collectgarbage()
 
 end
 
 function Settlements:Enter(args)
-
 	Events:Fire("PlayerEnterSettlement", {
 		settlement = self.settlements[args.id],
 		player = args.player
 	})
-
 end
 
 function Settlements:Exit(args)
-
 	Events:Fire("PlayerExitSettlement", {
 		settlement = self.settlements[args.id],
 		player = args.player
 	})
-
 end
+
+Events:Subscribe('PlayerEnterSettlement', function(args)
+	tprint(args)
+end)
 
 Settlements = Settlements()
